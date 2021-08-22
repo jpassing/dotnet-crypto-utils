78f0ff1721787fd91548c65651377f033acd73b1
Johannes Passing <jpassing@hotmail.com>
2021-08-22 09:39:31 +0200

    Import, export RSA public keys

---
+++ CryptoUtils.Test/TestPem.cs
@@ -0,0 +1,61 @@
+﻿using NUnit.Framework;
+using System;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace CryptoUtils.Test
+{
+    [TestFixture]
+    public class TestPem
+    {
+        private const string RsaPublicKey =
+            @"-----BEGIN RSA PUBLIC KEY-----
+            MIIBigKCAYEAq3DnhgYgLVJknvDA3clATozPtjI7yauqD4/ZuqgZn4KzzzkQ4BzJ
+            ar4jRygpzbghlFn0Luk1mdVKzPUgYj0VkbRlHyYfcahbgOHixOOnXkKXrtZW7yWG
+            jXPqy/ZJ/+kFBNPAzxy7fDuAzKfU3Rn50sBakg95pua14W1oE4rtd4/U+sg2maCq
+            6HgGdCLLxRWwXA8IBtvHZ48i6kxiz9tucFdS/ULvWsXjQnyE5rgs3tPhptyl2/js
+            /6FGgdKDaPal8/tud/rPxYSuzBPp7YwRKRRN1EpYQdd4tZzeXdvOvrSIfH+ZL7Rc
+            i+HGasbRjCom3HJL+wDGVggUkeuOUzZDjKGqZNCvZIqe5FuU0NAd8c2w2Mxaxia9
+            1G8jZDu92DqCEI/HoxXsZPSjd0L4EMx5HqXpYpFY2YPL95zabmynO3RCTWFN7uq6
+            DJGlzRCTHeRDa4CvNwHCzv0kqR4uo6VlWp2dW2M/v0k1+kP70EwGqq9dnK5RMXC3
+            XwJbrAbpGUDlAgMBAAE=
+            -----END RSA PUBLIC KEY-----";
+
+        private const string EccPublicKey =
+            @"-----BEGIN PUBLIC KEY-----
+            MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEAACGNU1rVGpVfFyfPlx4Ydz0pQ0N
+            2BCrIQpSccUmJbg6v1WYfYZNR9RAQuaONRAla0dhLC6NZ7oslIEW8iNdjA==
+            -----END PUBLIC KEY-----";
+
+        [Test]
+        public void WhenKeyHasWrongHeader_ThenToRsaPublicKeyThrowsException()
+        {
+            Assert.Throws<FormatException>(
+                () => Pem.FromRsaPublicKey(string.Empty));
+
+            Assert.Throws<FormatException>(
+                () => Pem.FromRsaPublicKey(EccPublicKey));
+        }
+
+        [Test]
+        public void WhenKeyValid_ThenToRsaPublicKeyReturnsKey()
+        {
+            var key = Pem.FromRsaPublicKey(RsaPublicKey);
+            Assert.IsNotNull(key);
+        }
+
+        [Test]
+        public void WhenRsaKeyExportedAndImported_ThenKeyIsSame()
+        {
+            var key = new RSACng();
+            var reimported = Pem.FromRsaPublicKey(key.ToRsaPublicKey());
+
+            Assert.IsTrue(Enumerable.SequenceEqual(
+                key.ExportParameters(false).Modulus,
+                reimported.ExportParameters(false).Modulus));
+            Assert.IsTrue(Enumerable.SequenceEqual(
+                key.ExportParameters(false).Exponent,
+                reimported.ExportParameters(false).Exponent));
+        }
+    }
+}
---
+++ CryptoUtils/Pem.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace CryptoUtils
+{
+    public static class Pem
+    {
+        public static string ToRsaPublicKey(this RSA key)
+        {
+            //
+            // Export key as DER-formatted blob.
+            //
+            byte[] derBlob;
+
+#if NET40_OR_GREATER
+            if (!(key is RSACng))
+            {
+                throw new NotImplementedException("Key must be a CNG key");
+            }
+
+            var keyBlob = ((RSACng)key).Key.Export(CngKeyBlobFormat.GenericPublicBlob);
+
+            var keyBlobNative = Marshal.AllocHGlobal(keyBlob.Length);
+            Marshal.Copy(keyBlob, 0, keyBlobNative, keyBlob.Length);
+
+            if (!UnsafeNativeMethods.CryptEncodeObjectEx(
+                UnsafeNativeMethods.X509_ASN_ENCODING |
+                    UnsafeNativeMethods.PKCS_7_ASN_ENCODING,
+                UnsafeNativeMethods.CNG_RSA_PUBLIC_KEY_BLOB,
+                keyBlobNative,
+                0,
+                IntPtr.Zero,
+                IntPtr.Zero,
+                out uint derBlobSize))
+            {
+                throw new Win32Exception(
+                    Marshal.GetLastWin32Error(),
+                    "Failed to encode RSA public key blob");
+            }
+
+            var derBlobNative = Marshal.AllocHGlobal((int)derBlobSize);
+            try
+            {
+                if (!UnsafeNativeMethods.CryptEncodeObjectEx(
+                    UnsafeNativeMethods.X509_ASN_ENCODING |
+                        UnsafeNativeMethods.PKCS_7_ASN_ENCODING,
+                    UnsafeNativeMethods.CNG_RSA_PUBLIC_KEY_BLOB,
+                    keyBlobNative,
+                    0,
+                    IntPtr.Zero,
+                    derBlobNative,
+                    out derBlobSize))
+                {
+                    throw new Win32Exception(
+                        Marshal.GetLastWin32Error(),
+                        "Failed to encode RSA public key blob");
+                }
+
+                derBlob = new byte[derBlobSize];
+                Marshal.Copy(derBlobNative, derBlob, 0, (int)derBlobSize);
+            }
+            finally
+            {
+                Marshal.FreeHGlobal(derBlobNative);
+            }
+
+#else
+            derBlob = key.ExportRSAPublicKey();
+#endif
+
+            //
+            // Wrap DER-formatted blob as PEM.
+            //
+            var buffer = new StringBuilder();
+            buffer.AppendLine("-----BEGIN RSA PUBLIC KEY-----");
+            buffer.AppendLine(Convert.ToBase64String(
+                derBlob,
+                Base64FormattingOptions.InsertLineBreaks));
+            buffer.AppendLine("-----END RSA PUBLIC KEY-----");
+
+            return buffer.ToString();
+        }
+
+        public static RSA FromRsaPublicKey(string pem)
+        {
+            if (!pem.StartsWith("-----BEGIN RSA PUBLIC KEY-----"))
+            {
+                throw new FormatException("Missing RSA Public key header");
+            }
+
+            //
+            // Extract DER-formatted blob.
+            //
+            var derBlob = Convert.FromBase64String(string.Concat(pem
+                .Split('\n')
+                .Select(s => s.Trim())
+                .Where(line => !line.StartsWith("-----"))));
+
+#if NET40_OR_GREATER
+
+            var derBlobNative = Marshal.AllocHGlobal(derBlob.Length);
+            Marshal.Copy(derBlob, 0, derBlobNative, derBlob.Length);
+
+            try
+            {
+                //
+                // Decode the key.
+                //
+                if (!UnsafeNativeMethods.CryptDecodeObjectEx(
+                    UnsafeNativeMethods.X509_ASN_ENCODING | 
+                        UnsafeNativeMethods.PKCS_7_ASN_ENCODING,
+                    UnsafeNativeMethods.RSA_CSP_PUBLICKEYBLOB,
+                    derBlobNative,
+                    (uint)derBlob.Length,
+                    0,
+                    IntPtr.Zero,
+                    IntPtr.Zero,
+                    out var keyBlobSize))
+                {
+                    throw new Win32Exception(
+                        Marshal.GetLastWin32Error(), 
+                        "Failed to extract RSA public key blob");
+                }
+
+                var keyBlob = Marshal.AllocHGlobal((int)keyBlobSize);
+                try
+                {
+                    if (!UnsafeNativeMethods.CryptDecodeObjectEx(
+                        UnsafeNativeMethods.X509_ASN_ENCODING | 
+                            UnsafeNativeMethods.PKCS_7_ASN_ENCODING,
+                        UnsafeNativeMethods.RSA_CSP_PUBLICKEYBLOB,
+                        derBlobNative,
+                        (uint)derBlob.Length,
+                        0,
+                        IntPtr.Zero,
+                        keyBlob,
+                        out keyBlobSize))
+                    {
+                        throw new Win32Exception(
+                            Marshal.GetLastWin32Error(), 
+                            "Failed to extract RSA public key blob");
+                    }
+
+                    //
+                    // Import the key blob.
+                    //
+                    var keyBlobBytes = new byte[keyBlobSize];
+                    Marshal.Copy(keyBlob, keyBlobBytes, 0, (int)keyBlobSize);
+                    return new RSACng(CngKey.Import(keyBlobBytes, CngKeyBlobFormat.GenericPublicBlob));
+                }
+                finally
+                {
+                    Marshal.FreeHGlobal(keyBlob);
+                }
+            }
+            finally
+            {
+                Marshal.FreeHGlobal(derBlobNative);
+            }
+#else
+
+            //
+            // Import key.
+            //
+            var key = RSA.Create();
+            key.ImportRSAPublicKey(derBlob, out var _);
+            return key;
+#endif
+        }
+    }
+}
