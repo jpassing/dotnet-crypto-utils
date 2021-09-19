ec1daab25caf8f0f89af05a4f59c7d24d92e844c
Johannes Passing <jpassing@hotmail.com>
2021-09-19 09:07:33 +0200

    Add copyright

--- CryptoUtils.Test/TestRSAExtensions.cs
+++ CryptoUtils.Test/TestRSAExtensions.cs
@@ -1,9 +1,27 @@
-﻿using NUnit.Framework;
-using System;
-using System.Collections.Generic;
+﻿//
+// Copyright 2021 Johannes Passing, https://jpassing.com/
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+// 
+//   http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.   
+// 
+
+using NUnit.Framework;
 using System.Linq;
 using System.Security.Cryptography;
-using System.Text;
 
 namespace CryptoUtils.Test
 {
@@ -120,35 +138,35 @@ namespace CryptoUtils.Test
         }
 
         [Test]
-        public void WhenPemContainsRSAPublicKey_ThenImportPublicKeySucceeds()
+        public void WhenPemContainsRSAPublicKey_ThenImportFromPemSucceeds()
         {
             var importedKey = CreateKey();
-            importedKey.ImportPublicKey(RsaPublicKeyPem, out var format);
+            importedKey.ImportFromPem(RsaPublicKeyPem, out var format);
 
             Assert.AreEqual(RsaPublicKeyFormat.RsaPublicKey, format);
-            var exported = importedKey.ExportPublicKey(format);
+            var exported = importedKey.ExportToPem(format);
             
             AssertPemEqual(RsaPublicKeyPem, exported);
         }
 
         [Test]
-        public void WhenPemContainsSubjectPublicKeyInfo_ThenImportPublicKeySucceeds()
+        public void WhenPemContainsSubjectPublicKeyInfo_ThenImportFromPemSucceeds()
         {
             var importedKey = CreateKey();
-            importedKey.ImportPublicKey(SubjectPublicKeyInfoPem, out var format);
+            importedKey.ImportFromPem(SubjectPublicKeyInfoPem, out var format);
 
             Assert.AreEqual(RsaPublicKeyFormat.SubjectPublicKeyInfo, format);
-            var exported = importedKey.ExportPublicKey(format);
+            var exported = importedKey.ExportToPem(format);
 
             AssertPemEqual(SubjectPublicKeyInfoPem, exported);
         }
 
         [Test]
-        public void WhenPemContainsEccPublicKey_ThenImportPublicKeyThrowsException()
+        public void WhenPemContainsEccPublicKey_ThenImportFromPemThrowsException()
         {
             var importedKey = CreateKey();
             Assert.Throws<CryptographicException>(
-                () => importedKey.ImportPublicKey(EccPublicKeyPem, out var format));
+                () => importedKey.ImportFromPem(EccPublicKeyPem, out var format));
         }
     }
 
---
+++ README.md
@@ -0,0 +1,9 @@
+# Crypto utility classes for .NET Framework
+
+`RSAExtensions` is a utility class that adds import and export functionality
+for PEM-encoded RSA public keys to .NET Framework and older .NET Core versions.
+
+See related blog posts:
+
+* [Importing RSA public keys using CryptoAPI and .NET]()
+* [Exporting RSA public keys using CryptoAPI and .NET]()
